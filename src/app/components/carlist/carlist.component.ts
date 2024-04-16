a31cc7d851b9ee6333cadda3ed1c99cb94b4ba84
ulskmo <128420582+ulaskmo@users.noreply.github.com>
2024-04-16 17:29:39 +0100

    lastrrttr

---
+++ src/app/components/carlist/carlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './carlist.component.html',
+  styleUrl: './carlist.component.css'
+})
+export class CarlistComponent {
+
+}
